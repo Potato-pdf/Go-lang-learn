fc88b5d56f074faad99eb0937fd837c584b99d22
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-10 13:06:55 -0600

    feat: datos

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main(){
 	fmt.Println("hola")
 }
+
+//Tipos de datos
+//int, int64, int32 | entero | siempre se use int a menos que se requiera control sobre el tamaño
+//float32, float64 | se usa para representar valores numericos reales(decimales) | 32 o 64 va asociado a la arquitectura del sistema
+//uint, uint16, uilt32, uint64 | entero sin signo (positivos unicamente)
+//bool |true/false | flag o condicional
+//string |"cadena de caracteres"
+//byte | === unit8 | se usa para datos binarios
+//rune | === int32 | se usa para representar un sol ocaracter que ocupa más de 1 byte
+//complex64, complex128 | cuando tiene una parte real y una imaginaria 
