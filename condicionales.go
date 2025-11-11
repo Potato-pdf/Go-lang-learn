9bd9a72834b5001e2659040f4fc0eff5e79f6791
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-11 10:31:22 -0600

    defer

--- condicionales.go
+++ condicionales.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func Condicional(){
 
+	//Defer
+	defer fmt.Println("Fin") //Se eejecuta al final, una vez que ya se ejecutara todo lo relacionado a la funcion en la que se encuentra
+
+
 }
