91921a732d52789c59b26b334b9435075512d1c2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-11 10:31:39 -0600

    if

--- condicionales.go
+++ condicionales.go
@@ -7,5 +7,10 @@ func Condicional(){
 	//Defer
 	defer fmt.Println("Fin") //Se eejecuta al final, una vez que ya se ejecutara todo lo relacionado a la funcion en la que se encuentra
 
-
-}
+	//If
+	edad :=20
+	if edad >= 18 {
+		fmt.Println("Eres mayor de edad")
+	}else {
+		fmt.Println("Eres menor de edad")
+	}
