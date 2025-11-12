a40abb3b2fc389c77c26422c513ed0908a5b436d
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-12 12:44:13 -0600

    feat: colas de mensajes

--- advanced-go/cap05/main.go
+++ advanced-go/cap05/main.go
@@ -1,2 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
 //
+func tarea(id int, canal chan <- string){
+	time.Sleep(time.Duration(id)*100*time.Millisecond)
+	canal<- fmt.Sprintf("Tarea %d terminada \n", id)
+}
+
+func main(){
+	canal:= make(chan string, 2)
+	for i:= range 4 {
+		go tarea(i, canal)
+
+	}
+	timeout := time.After(1 * time.Second)
+	for range 4 {
+		select {
+		case msj := <-canal:
+			fmt.Printf(msj)
+		
+		case <- timeout:
+		fmt.Println("Tiempo de espera agotado")
+	}
+	}
+}
