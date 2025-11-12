388b772d29eb410fba2331eef26b754a5f5750b1
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-12 09:38:49 -0600

    chanels

---
+++ channels.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//go -> GoRoutine
+//GoRutine -> es un hilo de ejecucion virtual
+//Chanell -> comuicacion entre GoRUtines
+
+func DecirHola(canal1 chan <- string){
+	time.Sleep(2 * time.Second)
+	canal1 <- "Hola Mundo"
+}
+
+func IMprimirMensaje(canal <- chan string){
+	mensaje := <- canal
+	fmt.Println(mensaje)
+}
