d519cd87d009cca9ccbf0d1297f87dc129c27cdf
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-12 11:43:12 -0600

    feat: mutex explicado a detalle

---
+++ advanced-go/cap 03/main.go
@@ -0,0 +1,33 @@
+package main
+
+// raceondition : ocurre cuando dos o mas go rutines intentan acceder y modificar una variable compartida al mismo tiempo
+//mutex : es un mecanismo de sincronizacion que permite asegurar que solo una go rutine pueda acceder a una seccion critica del codigo a la vez
+import "fmt"
+import "sync"
+
+var (
+	completadas int
+	mu sync.RWMutex
+)
+
+func tarea(id int, canal chan <- string){
+	mu.Lock()
+	completadas ++
+	mu.Unlock()
+	canal<-fmt.Sprintf("tarea %d completaa", id )
+}
+
+func main() {
+	canal := make(chan string)
+
+	for i := range 5{
+		go tarea(i, canal)
+	}
+
+	for range 5 {
+		fmt.Println(<-canal)
+	}
+	mu.Lock()
+	fmt.Printf("Total tareas completadas: " ,completadas)
+	mu.Unlock()
+}
