bfba838050f9fe5406fd686ff6b5c6a572852cf7
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-12 09:56:49 -0600

    feat: mutex

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func ImprimiArea(f Forma) {
 	fmt.Printf("El area es: %.2f \n", f.Area())
@@ -56,7 +60,30 @@ func main(){
 	for valor := range canal2 {
 		fmt.Println(valor)
 	}
-	
-}
 
+	//mutex
+	var contador int
+	var mu sync.RWMutex
+
+	//Writer
+	go func (){ 
+		for i := 0 ; i < 5 ; i ++ { 
+			mu.Lock() //Bloqueo de recursos mientras se escribe 
+			contador ++
+			mu.Unlock()
+		}
+	}()
+	//Reader
+	for i :=0 ; i < 5 ; i ++ {
+		go func (id int)  {
+			for j:=0 ; j < 5 ; j ++ {
+				mu.RLock()
+				fmt.Printf("Goroutine %d: %d\n", id, contador)
+				mu.RUnlock()
+				time.Sleep(100*time.Millisecond)
+			}
+		}(i)		
+	}
+	time.Sleep(2* time.Second)
+}
 
